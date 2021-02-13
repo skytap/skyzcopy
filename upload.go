5cfabe3700061a500c255851f4e2432f4527c11f
Aaron Mulgrew <mulgrewaaron1@gmail.com>
2021-02-13 22:08:18 +0000

    Convert all spaces to tabs

--- upload.go
+++ upload.go
@@ -63,11 +63,11 @@ func handleErrors(err error) {
 func main() {
 
 	// First element in os.Args is always the program name,
-    // So we need at least 2 arguments to have a file name argument.
-    if len(os.Args) < 2 {
-        fmt.Println("Missing parameter, usage as follows: \n ./skyzcopy_upload <filename_or_directory> [container_name]")
-        return
-    }
+	// So we need at least 2 arguments to have a file name argument.
+	if len(os.Args) < 2 {
+		fmt.Println("Missing parameter, usage as follows: \n ./skyzcopy_upload <filename_or_directory> [container_name]")
+		return
+	}
 	fileNameorDirectory := os.Args[1]
 
 
